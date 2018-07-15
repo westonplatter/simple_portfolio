41851c6135d9e48b7b69066d418efc7c9338796d
Weston Platter <westonplatter@gmail.com>
2018-07-15 08:16:27 -0600

    export stock orders. create the export logic.

--- simple_metrics/export.py
+++ simple_metrics/export.py
@@ -23,6 +23,22 @@ def option_orders(option_orders, options = {}):
         print("I/O error")
 
 
+def stock_orders(stock_orders, options = {}):
+    drows = stock_orders
+    cols = [*drows[0].keys()]
+    headers = ordered_stock_cols(cols)
+    filename = "stock_orders.csv"
+
+    try:
+        with open(filename, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=headers)
+            writer.writeheader()
+            for drow in drows:
+                writer.writerow(drow)
+    except IOError:
+        print("I/O Error")
+
+
 def order_option_cols(cols):
     expected_headers_ordered = [
         "id", "chain_id", "ref_id",  "updated_at", "created_at", "time_in_force",
@@ -31,6 +47,21 @@ def order_option_cols(cols):
         "opening_strategy", "closing_strategy", "legs",
         "trigger", "response_category",
         "cancel_url", "canceled_quantity",
+    ]
+
+    if set(cols) == set(expected_headers_ordered):
+        return expected_headers_ordered
+    else:
+        return cols
+
+
+def ordered_stock_cols(cols):
+    expected_headers_ordered = [
+        'id', 'ref_id', 'instrument', 'updated_at', 'created_at', 'last_transaction_at',
+        'time_in_force', 'trigger', 'cancel', 'response_category','state', 'reject_reason',
+        'type', 'side', 'price', 'stop_price', 'average_price', 'quantity', 'cumulative_quantity', 'fees',
+        'executions', 'extended_hours', 'account', 'url', 'position',
+        'override_day_trade_checks', 'override_dtbp_checks',
     ]
 
     if set(cols) == set(expected_headers_ordered):
