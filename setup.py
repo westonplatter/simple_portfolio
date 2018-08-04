fd8b44011d65e2f1f1900e8d31d7649ca4e3cdbd
Weston Platter <westonplatter@gmail.com>
2018-08-04 10:25:55 -0600

    resolves #4. move off of Robinhood client.

--- setup.py
+++ setup.py
@@ -14,14 +14,7 @@ setup(name='simple_metrics',
     packages=['simple_metrics'],
     install_requires=[
         'click',
-        'Robinhood==1.0.1',
-        'fast_arrow==0.0.2',
-    ],
-    dependency_links=[
-        # # this git commit hash is master as of July 14th. It's 64 commits ahead of release 1.0.1
-        # 'https://github.com/Jamonek/Robinhood/archive/d21b1907dfa0ec9ba04177e597350b7d2a1ef31e.zip#egg=Robinhood-1.0.1',
-
-        'https://github.com/westonplatter/Robinhood/archive/6294a5c3d53dd7553c05c272ebeb069df1c86aa2.zip#egg=Robinhood-1.0.1',
+        'fast_arrow==0.0.03',
     ],
     entry_points='''
         [console_scripts]
--- simple_metrics/fetch.py
+++ simple_metrics/fetch.py
@@ -1,85 +1,52 @@
-from Robinhood import Robinhood
-import fast_arrow
-
-
-def fetch_json_by_url(client, url):
-    return client.session.get(url).json()
+from fast_arrow import (
+    Auth,
+    StockOrder,
+    StockPosition,
+    OptionPosition,
+    OptionOrder
+)
 
 
 def stock_orders(account, options={}):
-    client = _get_client(account)
-
-    stock_orders = []
-    more_orders = True
-
-    res = client.order_history()
-    stock_orders.extend(res['results'])
-    cursor = res['next']
-    while cursor:
-        res = fetch_json_by_url(client, cursor)
-        stock_orders.extend(res['results'])
-        cursor = res['next']
-
-    return stock_orders
+    token = _get_token(account["username"], account["password"])
+    orders = StockOrder.all(token)
+    if ("only_filled" in options) and options["only_filled"]:
+        orders = list(filter(lambda x: x["state"] == "filled", orders))
+    return orders
 
 
 def option_orders(account, options={}):
-    client = _get_client(account)
-
-    options_orders = []
-    more_orders = True
-
-    res = client.options_order_history()
-    options_orders.extend(res['results'])
-    cursor = res['next']
-
-    while cursor:
-        res = fetch_json_by_url(client, cursor)
-        options_orders.extend(res['results'])
-        cursor = res['next']
-
-    return options_orders
+    token = _get_token(account["username"], account["password"])
+    orders = OptionOrder.all(token)
+    if ("only_filled" in options) and options["only_filled"]:
+        orders = list(filter(lambda x: x["state"] == "filled", orders))
+    return orders
 
 
 def positions(account, options={}):
-    client = _get_client(account)
+    token = _get_token(account["username"], account["password"])
+    ps = StockPosition.all(token)
+    if ("only_open" in options) and options["only_open"]:
+        ps = list(filter(lambda x: float(x["quantity"]) > 0.0, ps))
+    return ps
 
-    positions = []
-
-    res = client.positions()
-    positions.extend(res['results'])
-    cursor = res['next']
-    while cursor:
-        res = fetch_json_by_url(client, cursor)
-        positions.extend(res['results'])
-        cursor = res['next']
-
-    for index,x in enumerate(positions):
-        data = fetch_json_by_url(client, x['instrument'])
-        positions[index]['instrument_data'] = data
-
-    return positions
 
 def option_positions(account, options={}):
-    """
-    use fast_arrow to fetch positions
-    """
-    username = account["username"]
-    password = account["password"]
-    from fast_arrow.resources.auth import Auth
-    token = Auth.login(username, password)
-
-    from fast_arrow.resources.option_position import OptionPosition
+    token = _get_token(account["username"], account["password"])
     all_option_positions = OptionPosition.all(token)
     open_option_positions = list(filter(lambda p: float(p["quantity"]) > 0.0, all_option_positions))
-
-    bearer = Auth.get_oauth_token(token)
+    bearer = _get_bearer(token)
     results = OptionPosition.append_marketdata(bearer, open_option_positions)
-
     return results
 
 
-def _get_client(account):
-    client = Robinhood()
-    client.login(username=account['username'], password=account['password'])
-    return client
+
+
+def _get_token(username, password):
+    token = Auth.login(username, password)
+    return token
+
+
+def _get_bearer(token):
+    bearer = Auth.get_oauth_token(token)
+    return bearer
